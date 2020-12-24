bb51afb6b691a236f9aacc2d11b00756a26a3293
quekcg <quekcg3107@gmail.com>
2020-12-24 12:32:30 +0800

    update index.js file: commenting registerServiceWorker statements

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root')); 
 reportWebVitals();
-registerServiceWorker();
+// registerServiceWorker();
